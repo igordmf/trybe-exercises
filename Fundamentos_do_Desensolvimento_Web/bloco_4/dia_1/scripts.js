d9f2761e0d97f88983036ce2c4cc0ead59788019
igordmf <igordanielmf@gmail.com>
2021-03-24 18:37:21 -0300

    Adiciona exercicio .js 1 e 2

---
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_1/scripts.js
@@ -0,0 +1,152 @@
+// ***-PARTE 1-***
+// console.log('Parte 1 \n');
+//let name = 'Igor';
+// const birthCity = 'Recife';
+// let birthYear = 1993;
+
+// birthYear = 2020;
+// // birthCity = 'Boa Viagem';
+
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// console.log('\n');
+
+// ***-PARTE 2-***
+// console.log('Parte 2 \n');
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge);
+
+// ***-PARTE 3-***
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = 2 * base + 2 * altura;
+// console.log('A área é ' + area + 'm.');
+// console.log('O perímetro é de ' + perimetro + 'm.');
+
+// ***-PARTE 4-***
+// let nota = 70;
+
+// if (nota >= 80) {
+//   console.log('Parabéns, você foi aprovada(o)!');
+// } else if (nota >= 60 && nota < 80) {
+//   console.log('Você está na nossa lista de espera');
+// } else if (nota < 60) {
+//   console.log('Você foi reprovada(o)');
+// }
+
+// ***-PARTE 5-***
+// let estado; //= prompt('Digite um estado: \n\n aprovada \n lista \n reprovada');
+
+// estado = 'aprovada';
+
+// switch (estado) {
+//   case 'aprovada':
+//     console.log('Parabéns, você foi aprovada(o)!');
+//     break;
+
+//   case 'lista':
+//     console.log('Você está na nossa lista de espera');
+//     break;
+
+//   case 'reprovada':
+//     console.log('Você foi reprovada(o)');
+//     break;
+
+//   default:
+//     console.log('não se aplica');
+//     break;
+//   }
+
+// // ***-Exercicio 1-***
+// //Adicao
+// let a = 8;
+// let b = 4;
+// let soma = a + b;
+// console.log('O resultado da soma é ' + soma + '.')
+
+// // ***-Exercicio 1-***
+// //Subtracao
+// let c = 8;
+// let d = 4;
+// let subtracao = c - d;
+// console.log('O resultado da subtracao é ' + subtracao + '.');
+
+// // ***-Exercicio 1-***
+// //Multiplicacao
+// let e = 8;
+// let f = 4;
+// let multiplicacao = e * f;
+// console.log('O resultado da multiplicacao é ' + multiplicacao + '.');
+
+// // ***-Exercicio 1-***
+// //Divisao
+// let g = 8;
+// let h = 4;
+// let divisao = g / h;
+// console.log('O resultado da divisao é ' + divisao + '.');
+
+// // ***-Exercicio 1-***
+// //Modulo
+// let i = 8;
+// let j = 4;
+// let modulo = i % j;
+// console.log('O módulo da divisao é ' + modulo + '.');
+
+// ***-Exercicio 2-***
+// let num1 = 7;
+// let num2 = 7;
+
+// if (num1 > num2) {
+//   console.log('O maior número é o ' + num1);
+// } else if (num1 < num2) {
+//   console.log('O maior número é o ' + num2);
+// } else if (num1 == num2) {
+//   console.log('O dois número são iguais');
+// } else {
+//   console.log('Não é possível verificar!');
+// }
+
+// ***-Exercicio 3-***
+let num1 = 7;
+let num2 = 7;
+let num3 = 10;
+
+if (num1 > num2) {
+  console.log('O maior número é o ' + num1);
+} else if (num1 < num2) {
+  console.log('O maior número é o ' + num2);
+} else if (num1 == num2) {
+  console.log('O dois número são iguais');
+} else {
+  console.log('Não é possível verificar!');
+}
+
+// ***-Exercicio 4-***
+
+// ***-Exercicio 5-***
+
+// ***-Exercicio 6-***
+
+// ***-Exercicio 7-***
+
+// ***-Exercicio 8-***
+
+// ***-Exercicio 9-***
+
+// ***-Exercicio 10-***
+
+// ***-Exercicio 11-***
