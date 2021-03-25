2ac8b843512cb4dcec257cd3e67e12bc39cd477c
igordmf <igordanielmf@gmail.com>
2021-03-25 16:06:56 -0300

    Adiciona exercicio 4

--- Fundamentos_do_Desensolvimento_Web/bloco_4/dia_2/scripts.js
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_2/scripts.js
@@ -64,16 +64,31 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(soma);
 
 //--- Exercicio 3 ---
+// let soma = 0;
+// for (index = 0; index < numbers.length; index += 1) {
+//   soma = soma + numbers[index];
+// }
+// let media = soma / numbers.length;
+
+// console.log(media);
+
+//--- Exercicio 4 ---
 let soma = 0;
+
 for (index = 0; index < numbers.length; index += 1) {
   soma = soma + numbers[index];
 }
+
 let media = soma / numbers.length;
 
-console.log(media);
-
-//--- Exercicio 4 ---
-
+switch (true) {
+  case (media > 20):
+    console.log('valor maior que 20');
+    break;
+  default:
+    console.log('valor menor ou igual a 20');
+    break;
+}
 
 //--- Exercicio 5 ---
 
