9a72dae980c217196767f6ae8c2cf5b6056a3547
igordmf <igordanielmf@gmail.com>
2021-03-25 15:36:29 -0300

    Adiciona exercicio 1

---
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_2/scripts.js
@@ -0,0 +1,73 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+
+// tasksList.push('Fazer exercícios da Trybe');
+// tasksList.unshift('Acordar');
+
+// tasksList.pop();
+
+// console.log(tasksList);
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato');
+
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+//   console.log(groceryList[index]);
+// }
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for (let name of names) {
+//   console.log(name);
+// }
+
+//--- Exercicios ---
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//--- Exercicio 1 ---
+for (index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+
+//--- Exercicio 2 ---
+
+
+//--- Exercicio 3 ---
+
+//--- Exercicio 4 ---
+
+//--- Exercicio 5 ---
+
+//--- Exercicio 6 ---
+
+//--- Exercicio 7 ---
+
+//--- Exercicio 8 ---
+
+//--- Exercicio 9 ---
