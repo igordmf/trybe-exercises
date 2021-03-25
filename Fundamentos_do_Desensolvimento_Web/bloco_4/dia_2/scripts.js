3fd13ca62acfa202eb39aa42ced2f6b02ea1eabe
igordmf <igordanielmf@gmail.com>
2021-03-25 15:57:04 -0300

    Adiciona exercicio 2

--- Fundamentos_do_Desensolvimento_Web/bloco_4/dia_2/scripts.js
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_2/scripts.js
@@ -51,12 +51,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //--- Exercicio 1 ---
-for (index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
-}
+// for (index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
 
 //--- Exercicio 2 ---
+let soma = 0;
+for (index = 0; index < numbers.length; index += 1) {
+  soma = soma + numbers[index];
+}
 
+console.log(soma);
 
 //--- Exercicio 3 ---
 
