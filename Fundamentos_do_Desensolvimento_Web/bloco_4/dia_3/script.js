da23fa0ae07c22d248d3739672906e071af78600
igordmf <igordanielmf@gmail.com>
2021-03-26 20:18:09 -0300

    Adiciona exercicio 4

--- Fundamentos_do_Desensolvimento_Web/bloco_4/dia_3/script.js
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_3/script.js
@@ -22,20 +22,35 @@
 // }
 
 //--- Exercicio 3 ---
-let n = 7;
+// let n = 7;
+// let stringDeAst = '*';
+
+// for (let i = 0; i < n; i += 1) {
+//   for(let j = 1; j < n; j += 1){
+//     if (j <= i) {
+//       stringDeAst = '*' + stringDeAst;
+//     } else {
+//       stringDeAst = ' ' + stringDeAst;
+//     }
+//   }
+//   console.log(stringDeAst);
+//   stringDeAst = '*';
+// }
+
+//--- Exercicio 4 ---
+
+let n = 11;
 let stringDeAst = '*';
 
-for (let i = 0; i < n; i += 1) {
-  for(let j = 1; j < n; j += 1){
-    if (j <= i) {
-      stringDeAst = '*' + stringDeAst;
+for (let i = 1; i <= ((n / 2) + 1); i += 1) {
+  for(let j = 1; j <= ((n-1) / 2); j += 1) {
+    if(j >= i) {
+      stringDeAst = ' ' + stringDeAst + ' ';
     } else {
-      stringDeAst = ' ' + stringDeAst;
+      stringDeAst = '*' + stringDeAst + '*';
     }
   }
   console.log(stringDeAst);
   stringDeAst = '*';
 }
 
-
-//--- Exercicio 4 ---
