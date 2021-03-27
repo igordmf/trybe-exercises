9b86510781b5bc9e2bcf30d1aa676f547253ab76
igordmf <igordanielmf@gmail.com>
2021-03-26 22:07:15 -0300

    Adiciona exercicio 6

--- Fundamentos_do_Desensolvimento_Web/bloco_4/dia_3/script.js
+++ Fundamentos_do_Desensolvimento_Web/bloco_4/dia_3/script.js
@@ -39,18 +39,65 @@
 
 //--- Exercicio 4 ---
 
-let n = 11;
-let stringDeAst = '*';
+// let n = 11;
+// let stringDeAst = '*';
 
-for (let i = 1; i <= ((n / 2) + 1); i += 1) {
-  for(let j = 1; j <= ((n-1) / 2); j += 1) {
-    if(j >= i) {
-      stringDeAst = ' ' + stringDeAst + ' ';
-    } else {
-      stringDeAst = '*' + stringDeAst + '*';
-    }
+// for (let i = 1; i <= ((n + 1) / 2) + 1); i += 1) {
+//   for(let j = 1; j <= ((n-1) / 2); j += 1) {
+//     if(j >= i) {
+//       stringDeAst = ' ' + stringDeAst + ' ';
+//     } else {
+//       stringDeAst = '*' + stringDeAst + '*';
+//     }
+//   }
+//   console.log(stringDeAst);
+//   stringDeAst = '*';
+// }
+
+//--- Exercicio 5 ---
+// let n = 11;
+// let stringDeAst = '*';
+
+// for (let k = 0; k < n; k += 1) {
+//   stringDeAst = ' ' + stringDeAst + ' ';
+// }
+// console.log(stringDeAst);
+
+
+// for (let i = 1; i <= ((n - 1) / 2); i += 1) {
+//   for(let j = 1; j <= ((n-1) / 2); j += 1) {
+//     if(j >= i) {
+//       stringDeAst = '*' + ' ' + stringDeAst;
+//     }
+//   }
+//   console.log(stringDeAst);
+//   stringDeAst = '*';
+// }
+// stringDeAst = "";
+// for (let k = 0; k < n; k += 1) {
+//   stringDeAst = stringDeAst + '*';
+// }
+// console.log(stringDeAst);
+
+// n = 7
+//    *
+//   * *
+//  *   *
+// *******
+
+//--- Exercicio 6 ---
+
+let ePrimo = 17781;
+let divisores = [1];
+
+for (divisor = 2; divisor < ePrimo; divisor += 1) {
+  if (ePrimo % divisor === 0) {
+    divisores.push(divisor);
   }
-  console.log(stringDeAst);
-  stringDeAst = '*';
 }
 
+if (divisores.length === 0) {
+  console.log('O numero ' + ePrimo + ' é um número primo.');
+} else {
+  console.log('O numero ' + ePrimo + ' não é um número primo.');
+}
